3313ed2aad745460394fdc740c6173886ee3c6dc
yuxinbin <459348471@qq.com>
2022-03-14 21:55:19 +0800

    测试提交

--- cloud-provider-payment8001/src/main/java/com.boys.springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com.boys.springcloud/PaymentMain8001.java
@@ -3,6 +3,9 @@ package com.boys.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动
+ */
 @SpringBootApplication
 public class PaymentMain8001{
 
