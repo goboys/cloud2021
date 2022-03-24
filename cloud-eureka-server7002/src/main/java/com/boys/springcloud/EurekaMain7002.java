fcbc031f799d4f59376cf166363b8d621fd8e651
yuxinbin <459348471@qq.com>
2022-03-24 22:47:01 +0800

    新增7002控制中心

--- cloud-eureka-server7002/src/main/java/com/boys/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/boys/springcloud/EurekaMain7002.java
@@ -2,19 +2,14 @@ package com.boys.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * 启动
- */
 @SpringBootApplication
-@EnableEurekaClient
-public class MainApp80 {
+@EnableEurekaServer
+public class EurekaMain7002 {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(MainApp80.class,args);
-
+        SpringApplication.run(EurekaMain7002.class,args);
     }
 
 }
--- cloud-provider-payment8001/src/main/java/com.boys.springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com.boys.springcloud/PaymentMain8001.java
@@ -2,11 +2,13 @@ package com.boys.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 启动
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001{
 
     public static void main(String[] args) {
