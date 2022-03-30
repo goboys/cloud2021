c8d343c1cf4bcd8f99e5a9a283f01c1acbc32950
yuxinbin <459348471@qq.com>
2022-03-30 23:22:45 +0800

    新增负载均衡功能

--- cloud-consumer-order80/src/main/java/com/boys/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/boys/springcloud/controller/OrderController.java
@@ -1,17 +1,20 @@
 package com.boys.springcloud.controller;
 
 import com.boys.springcloud.entities.CommonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.beans.factory.annotation.Value;
 import javax.annotation.Resource;
 
 @RestController
+@Slf4j
 public class OrderController {
 
-    public static final String PaymentSrv_URL = "http://localhost:8001";
+    public static final String PaymentSrv_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
@@ -20,7 +23,6 @@ public class OrderController {
     public CommonResult getPaymentById(@PathVariable Long id){
 
         CommonResult result =  restTemplate.getForObject(PaymentSrv_URL+"/payment/get/"+id,CommonResult.class,id);
-
         return result;
     }
 }
--- cloud-provider-payment8001/src/main/java/com.boys.springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com.boys.springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.boys.springcloud.entities.CommonResult;
 import com.boys.springcloud.entities.Payment;
 import com.boys.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -15,6 +16,9 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment)
     {
@@ -23,7 +27,7 @@ public class PaymentController {
 
         if(result > 0)
         {
-            return new CommonResult(200,"插入数据库成功",result);
+            return new CommonResult(200,"插入数据库成功,端口="+serverPort,result);
         }else{
             return new CommonResult(444,"插入数据库失败",null);
         }
@@ -36,7 +40,7 @@ public class PaymentController {
         log.info("*****查询结果:{}",payment);
         log.info("测试是否热部署");
         if (payment != null) {
-            return new CommonResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功,端口="+serverPort,payment);
         }else{
             return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
         }
